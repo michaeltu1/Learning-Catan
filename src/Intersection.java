cce964c3fc4c60df5a1c3623c57860dc1372d732
alexyip999 <35481543+alexyip999@users.noreply.github.com>
2018-03-09 16:27:45 -0800

    settlement placing (except by ocean)

--- src/Intersection.java
+++ src/Intersection.java
@@ -15,18 +15,37 @@ public class Intersection {
     public boolean canSettle() {
         if (hasBuilding) {
             return false;
-        } else if () {
+        } else if (neighborSettlement()) {
             return false;
         }
         return true;
     }
 
-    public void checkAdjacents() {
-        for (adjacents) {
-            Board.adjacentTiles.
+    public boolean neighborSettlement(Intersection neighbor) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (this.adjacents.get(i) == neighbor.adjacents.get(j)) {
+//                    for (int k = i + 1; k < 3; k++) {
+//                        for (int l = j + 1; l < 3; l++) {
+//                            if (this.adjacents.get(k) == neighbor.adjacents.get(l)) {
+//                                if (neighbor.hasBuilding) {
+//                                    return true;
+//                                }
+//                            }
+//                        }
+//                    }
+                    i = i + 1;
+                }
+                if (this.adjacents.get(i) == neighbor.adjacents.get(j)) {
+                    if (neighbor.hasBuilding) {
+                        return true;
+                    }
+                }
+            }
         }
-
+        return false;
     }
+
 
     public static void main(String[] args) {
 
