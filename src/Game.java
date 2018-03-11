6d3c7fc036d06be30a909f1cc06b03eb26609d8a
Michael Tu <mtu@berkeley.edu>
2018-03-11 01:05:44 -0800

    removed extra print statements

--- src/Game.java
+++ src/Game.java
@@ -4,17 +4,10 @@ public class Game {
 
     public int rollDice() {
         Random r = new Random();
-        int roll = r.nextInt(7) + r.nextInt(7);
-        System.out.print("dice roll = ");
-        System.out.println(roll);
-//        return r.nextInt(7) + r.nextInt(7);
-        return roll;
+        return r.nextInt(7) + r.nextInt(7);
     }
 
     public static void main(String[] args) {
         Game g = new Game();
-        g.rollDice();
-        g.rollDice();
-        g.rollDice();
     }
 }
