3bec62d802baec7ca7da775b08b6c3f3e5a30e73
Michael Tu <mtu@berkeley.edu>
2018-03-11 01:13:48 -0800

    discuss how Game.nextTurn() will work :/

--- src/Game.java
+++ src/Game.java
@@ -2,6 +2,22 @@ import java.util.Random;
 
 public class Game {
 
+    public void nextTurn() {
+        /*
+        TODO:
+        playing dev cards
+        roll dice
+        distr. resources (if possible)
+        discard half the hand on 7's
+        move robber
+
+        building phase
+
+        return value?
+         */
+        int diceRoll = rollDice();
+    }
+
     public int rollDice() {
         Random r = new Random();
         return r.nextInt(7) + r.nextInt(7);
